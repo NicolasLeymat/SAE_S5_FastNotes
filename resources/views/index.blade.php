4c8afce12dc07c1d21642e7f23d71e8c373083d1
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-10-20 08:24:07 +0200

    fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -71,7 +71,7 @@
               <p>Admin et Prof</p>
               <a class="Entreprise button button-order" href="{{ route('evaluations') }}"> Accéder à la dashboard profésseur </a>
               <a class="Entreprise button button-order" > Accéder à la dashboard Admin </a>
-              <a class="Entreprise button button-order" href="/visualisation/{{Auth::user()->identification}}"> Accéder à la visualitation des notes </a>
+              <a class="Entreprise button button-order" href="/visualisation/{{Auth::user()->code}}"> Accéder à la visualitation des notes </a>
               @elseif (Auth::user()->isAdmin)
               <p>Admin</p>
               <a class="Entreprise button button-order" href=""> Accéder à la dashboard Admin </a>
@@ -80,7 +80,7 @@
               <a class="Entreprise button button-order" href="{{ route('evaluations') }}"> Accéder à la dashboard profésseur </a>
               @else
                 <p>Éleve</p>
-                <a class="Entreprise button button-order" href="/visualisation/{{Auth::user()->identification}}"> Accéder à la visualitation des notes </a>
+                <a class="Entreprise button button-order" href="/visualisation/{{Auth::user()->code}}"> Accéder à la visualitation des notes </a>
               @endif
             @endauth
           </div>

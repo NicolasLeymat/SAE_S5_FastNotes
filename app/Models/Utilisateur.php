5b5db3faf111b4871e88d374d2f21a58596f9f92
NicolasLeymat <74655132+NicolasLeymat@users.noreply.github.com>
2023-10-20 11:12:24 +0200

    Sprint 1

---
+++ app/Models/Utilisateur.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Utilisateur extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    public $primaryKey = 'code';
+    public $incrementing = false;
+    protected $fillable = [
+        'code',
+        'identification',
+        'password',
+        'email',
+        'nom',
+        'prenom',
+        'isProf',
+        'isAdmin',
+        'id_groupe'
+    ];
+
+    protected $table = "users";
+
+    public function evaluations() {
+        return $this->belongsToMany (Evaluation::class, 'note_evaluation', 'code_eleve', 'id_evaluation')
+        ->withPivot("note");
+    }
+
+    public function groupe() {
+        return $this->belongsTo(Groupe::class);
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+}
---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\EleveController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EvaluationController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/dashprof', function () {
+    return view('dashprof');
+})->name('dashprof');
+
+Route::get('/evaluation', function () {
+    return view('evaluation');
+})->name('evaluation');
+
+Route::get('/visuNote', function () {
+    return view('visuNote');
+})->name('visuNote');
+
+Route::post('saisir_note',[EvaluationController::class, 'saisirNote'])->name('saisir_note');
+Route::post('saisir_notes',[EvaluationController::class, 'saisirNotes'])->name('saisir_notes');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::resource('evaluation', EvaluationController::class);
+Route::resource('evaluation', EvaluationController::class)->name("index","evaluations");
+
+Route::resource('visualisation', EleveController::class);

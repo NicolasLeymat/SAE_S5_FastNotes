91be86266ceb7059241a50fe42508d6298a443c1
Louis Camborieux PC Portable <louis.camborieux@etu.iut-tlse3.fr>
2023-10-27 11:28:29 +0200

    nouveaux tests  + competenceFactory

--- tests/Feature/UtilisateurTest.php
+++ tests/Feature/UtilisateurTest.php
@@ -5,7 +5,14 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\Utilisateur;
+use App\Models\Evaluation;
+use Illuminate\Support\Facades\Gate;
+use App\Http\Controllers\EleveController;
+use App\Models\Competence;
+use App\Models\Ressource;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
 
 class UtilisateurTest extends TestCase
 {
@@ -40,6 +47,51 @@ class UtilisateurTest extends TestCase
         $response = $this->actingAs($user)->get('visualisation/'.$user2->code);
 
         $response->assertStatus(403);
+
+    }
+
+    public function test_moyenne(): void
+    {
+        Gate::define('isEleve', function ($user) {
+            return true;
+        });
+
+        Gate::define('isAdmin', function ($user) {
+            return true;
+        });
+
+
+        $user = Utilisateur::factory()->create();
+        $competence = Competence::factory()->create();
+        $ressource = Ressource::factory()->create();
+        
+        $ressource->competences()->syncWithoutDetaching([
+            $competence->code => ['coefficient' => 1]
+        ]);
+        
+
+        $eval = Evaluation::factory()->create(['coefficient' => 1, 'code_ressource' => $ressource]);
+        $eval2 = Evaluation::factory()->create(['coefficient' => 1,'code_ressource' => $ressource ]);
+        
+        
+        $eval->utilisateurs()->syncWithoutDetaching([
+            $user->code => ['note' => 10]
+        ]);
+
+        $eval2->utilisateurs()->syncWithoutDetaching([
+            $user->code => ['note' => 5]
+        ]);
+
+         
+        $controller = app(EleveController::class);
+        $code = $competence->code;
+        try {
+        $moyenne = $controller->moyenneSemestre($user->code);
+        } catch (\Exception $e) {
+            // Capturez l'exception et affichez ses détails
+            $this->assertTrue(true, $e->getMessage());
+        }
+        
 
     }
 }

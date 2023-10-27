61c781c364a98f33a2a156cd5d8332c283faf182
Louis Camborieux PC Portable <louis.camborieux@etu.iut-tlse3.fr>
2023-10-27 10:02:36 +0200

    tests utilisateurs

--- tests/Feature/UtilisateurTest.php
+++ tests/Feature/UtilisateurTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Utilisateur;
 use Tests\TestCase;
 
 class UtilisateurTest extends TestCase
@@ -11,10 +12,34 @@ class UtilisateurTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_guest_cant_see_notes(): void
     {
-        $response = $this->get('/');
+        $user = Utilisateur::factory()->create();
+
+        $response = $this->get('visualisation/'.$user->code);
+
+        $response->assertStatus(403);
+
+    }
+
+    public function test_eleve_can_see_his_notes(): void
+    {
+        $user = Utilisateur::factory()->create();
+
+        $response = $this->actingAs($user)->get('visualisation/'.$user->code);
 
         $response->assertStatus(200);
+
+    }
+
+    public function test_eleve_can_only_see_his_notes(): void
+    {
+        $user = Utilisateur::factory()->create();
+        $user2 = Utilisateur::factory()->create();
+
+        $response = $this->actingAs($user)->get('visualisation/'.$user2->code);
+
+        $response->assertStatus(403);
+
     }
 }

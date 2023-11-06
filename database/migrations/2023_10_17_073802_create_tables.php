9d79048921b2c4ce549e6d20f5c7859b68eff8cd
NicolasLeymat <74655132+NicolasLeymat@users.noreply.github.com>
2023-11-06 09:35:42 +0100

    fix migration after changing groupe id type

--- database/migrations/2023_10_17_073802_create_tables.php
+++ database/migrations/2023_10_17_073802_create_tables.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {       
         Schema::create('groupe', function (Blueprint $table) {
-            $table->id();
+            $table->string('id');
+            $table->primary('id');
             $table->string('libelle');
             $table->string('semestre');
             $table->year('annee');

b0fae2666ecf4e1443a2eab8dc329538f942371f
NicolasLeymat <74655132+NicolasLeymat@users.noreply.github.com>
2024-01-12 11:13:52 +0100

    Ajout des nom et prenoms pour la page afficherEleves

--- resources/views/afficherEleves.blade.php
+++ resources/views/afficherEleves.blade.php
@@ -7,17 +7,23 @@
                 <thead class="tab-row-dark">
                   <tr>   
                     <th>Code élève</th>
+                    <th>Nom élève</th>
+                    <th>Prenom élève</th>
                     <th>Groupe</th>
                     <th>Parcours</th>
+                    <th></th>
                     <th></th>
                   </tr> 
                 </thead>
                 @for ($i = 0; $i < count($tabEleves); $i++)
                   <tr class="tab-row tab-row-clear">
                     <td class="tab-cell" >{{ $tabEleves[$i]->code}}</td>
+                    <td class="tab-cell" >{{ $tabEleves[$i]->utilisateur->nom}}</td>
+                    <td class="tab-cell" >{{ $tabEleves[$i]->utilisateur->prenom}}</td>
                     <td class="tab-cell "> {{ $listeGroupes[$i]->libelle }} </td>
                     <td class="tab-cell" >{{ $listeGroupes[$i]->parcours }}</td>
-                    <td><a class="clear-cell button del-button " href="#">Supprimer </a> </td>
+                    <td class="tab-cell" ><a class="clear-cell button del-button " href="/pdf/{{ $tabEleves[$i]->code }}"> Télécharger le relevé de notes </a></td>
+                    <td><a class="clear-cell button del-button " href="#"> Supprimer </a> </td>
                   </tr>
                 @endfor
             </table>

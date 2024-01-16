59714c95155f42d880cc1fe123e21e649a5358c3
Louis Camborieux PC Portable <louis.camborieux@etu.iut-tlse3.fr>
2024-01-16 09:02:07 +0100

    Fix Evaluation Controller

--- app/Http/Controllers/GroupeController.php
+++ app/Http/Controllers/GroupeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Groupe;
+use App\Models\Parcours;
 
 class GroupeController extends Controller
 {
@@ -11,5 +12,11 @@ class GroupeController extends Controller
         $tabGroupes = Groupe::paginate(10);
         
         return view('afficherGroupes', compact('tabGroupes'));
+    }
+
+    public function create() {
+        $listeParcours = Parcours::all();
+
+        return view ('ajoutGroupe',compact('listeParcours'));
     }
 }

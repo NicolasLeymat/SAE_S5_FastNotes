98af0c915d721df96bab3fcbde70e3b7955aa8b6
mederic.demailly <med.demailly@gmail.com>
2024-01-16 09:45:54 +0100

    Suppression d'une ressourc et des entités liées

--- routes/web.php
+++ routes/web.php
@@ -120,7 +120,7 @@ Route::middleware('administrateur')->group(function () {
     Route::delete('supprimerEnseignement',[EnseignementController::class, 'destroy' ])->name('supprimerEnseignement');
     Route::delete('supprimerEval',[EvaluationController::class, 'destroy' ])->name('supprimerEval');
     Route::delete('supprimerEleve',[EleveController::class, 'destroy' ])->name('supprimerEleve');
-    
+    Route::delete('supprimerRessource',[RessourceController::class, 'destroy' ])->name('supprimerRessource');
 });
 
 Route::get('pdf/{id}', [EleveController::class, 'exportBulletinPDF'])->name('pdf');

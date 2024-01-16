657fd1faa5b7dc17ff3964f0060d0ad63901fe07
mederic.demailly <med.demailly@gmail.com>
2024-01-16 10:08:22 +0100

    Suppression d'un groupe et tout ce qui est lié

--- app/Http/Controllers/GroupeController.php
+++ app/Http/Controllers/GroupeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Groupe;
+use DB;
 
 class GroupeController extends Controller
 {
@@ -11,5 +12,28 @@ class GroupeController extends Controller
         $tabGroupes = Groupe::paginate(10);
         
         return view('affichage_elements.afficherGroupes', compact('tabGroupes'));
+    }
+
+    public function destroy(Request $request) {
+        $groupeId = $request->input('groupe');
+
+        $groupe = Groupe::findOrFail($groupeId);
+
+        foreach($groupe->eleves as $eleve){
+            $eleve->id_groupe = null;
+            $eleve->save();
+        }
+
+        $req = DB::table('enseignements')
+        ->where('id_groupe',$groupeId)
+        ->delete();
+
+        $req = DB::table('ressource_groupe')
+        ->where('id_groupe',$groupeId)
+        ->delete();
+
+        $groupe->delete();
+
+        return redirect()->back()->with('message', 'Suppression effectuée avec succès.');
     }
 }
--- resources/views/affichage_elements/afficherGroupes.blade.php
+++ resources/views/affichage_elements/afficherGroupes.blade.php
@@ -14,7 +14,11 @@
                   <tr class="tab-row tab-row-clear">
                     <td class="tab-cell" >{{ $tabGroupes[$i]->id}}</td>
                     <td class="tab-cell" >{{ $tabGroupes[$i]->parcours}}</td>  
-                    <td><a class="clear-cell button del-button " href="#">Supprimer </a> </td>
+                    <form method="post" action = "{{route ('supprimerGroupe', ['groupe'=>$tabGroupes[$i]->id]) }}">
+                      @csrf
+                      @method('DELETE')
+                      <td class="tab-cell "><button class="tab-cell clear-cell del-button " type="submit">Supprimer </button> </td>
+                    </form>
                   </tr>
                 @endfor
             </table>

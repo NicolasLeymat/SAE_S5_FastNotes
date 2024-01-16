61c7459f5c1974b311fcd1e0194636040a30d857
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2024-01-16 09:12:11 +0100

    ajout simple parcours

--- app/Http/Controllers/ParcoursController.php
+++ app/Http/Controllers/ParcoursController.php
@@ -17,4 +17,31 @@ class ParcoursController extends Controller
         }
         return view('affichage_elements.afficherParcours', compact('tabParcours','listeSemestres'));
     }
+
+    public function create() {
+        $listeSemestres = Semestre::all();
+
+        return view('ajouts.ajoutParcours',compact('listeSemestres'));
+    }
+
+    public function store(Request $request){
+
+        $customErrorMessages = [
+            'required' => 'Le champ :attribute est requis.',
+            'min' => 'Le champ :attribute doit contenir au moins :min caractères.',
+            'regex' => 'Le champ :attribute doit contenir au moins une majuscule et un chiffre.',
+            'same' => 'Les mots de passe doivent être identiques',
+            'unique' => 'Ce code est déja utilisé'
+        ];
+
+        $validator =  $request ->validate([
+            'id' => 'required|string|max:255',
+            'semestre'=>'required|string|max:255'],
+            $customErrorMessages
+        );
+
+        $parcours = Parcours::create(['id_parcour' => $request->input('id'), 'id_semestre' => $request->input('semestre')]);
+
+        return redirect()->route('parcours.index')->withErrors($validator);
+    }
 }

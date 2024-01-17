3aa522c53fe86a2b8bf934374c4a16cac933317f
Louis Camborieux PC Portable <louis.camborieux@etu.iut-tlse3.fr>
2024-01-17 19:29:43 +0100

    redirection quand session expirée/ non connecté

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,37 +29,7 @@
 <body>
 
     <!--  HEADER  -->
-    <header class="header" id="header">
-    <nav class="nav container">
-        <a href="{{ route('index') }}" class="nav_logo"
-        >Fast
-        Notes</a
-        >
-        <div class="nav_menu" id="nav-menu">
-        <ul class="nav_list grid">
-          @auth 
-          <li class="nav_item">
-            <form method="POST" action="{{ route('logout') }}">
-              @csrf
-              <x-dropdown-link :href="route('logout')"
-                  onclick="event.preventDefault();
-                    this.closest('form').submit();">
-                <p class="nav_link">{{ __('Se déconnecter') }}</p>
-              </x-dropdown-link>
-            </form>
-          </li>
-          @endauth
-        </ul>
-        <i class="uil uil-times nav_close" id="nav-close"></i>
-        </div>
-        <div class="nav_btns">
-        <i class="uil uil-moon change-theme" id="theme-button"></i>
-        <div class="nav_toggle" id="nav-toggle">
-            <i class="uil uil-apps"></i>
-        </div>
-        </div>
-    </nav>
-    </header>
+    @include ('partials.header')
     <!--  HEADER FIN  -->
 
     <!--  MAIN   -->

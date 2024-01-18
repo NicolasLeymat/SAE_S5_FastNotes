b9fa2d75ff58b66b729aaf0ceb128eb496dfc5dc
vsl4346a <lucas.veslin@etu.iut-tlse3.fr>
2024-01-18 10:23:29 +0100

    la fin

--- resources/views/emails/notif_note.blade.php
+++ resources/views/emails/notif_note.blade.php
@@ -9,7 +9,7 @@
 <body>
     <p>Bonjour {{ $prenom }} {{ $nom }},</p>
     
-    <p>Nous vous informons que vous avez obtenu la note de {{ $note }} en {{ $libelle_eval }}.</p>
+    <p>Nous vous informons que vous avez obtenu la note de "{{ $note }}" en "{{ $libelle_eval }}".</p>
 
     <p>Vous pouvez consulter vos résultats sur notre plateforme : <a href="http://127.0.0.1:8000">Fast Notes</a>.</p>
 
